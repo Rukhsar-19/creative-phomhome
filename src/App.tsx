cb53f883b48fc021179188f7c1c9b52119d2ed8e
Rukhsar-19 <riazrukhsarriaz@gmail.com>
2025-12-15 12:50:27 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'remixicon/fonts/remixicon.css'
 import "./Styles/Commonstyles.css";
 import Header from './Components/Header';
